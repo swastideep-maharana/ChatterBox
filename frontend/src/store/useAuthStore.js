9e169b076370de9ab3097b848a8bb503f4553bcb
swastideep maharana <swastideep67maharana@gmail.com>
2024-12-18 21:19:05 +0530

    signup,login,home,setting page done

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,43 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+
+
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIng: false,
+    isUpdatingProfile: false,
+
+    isCheckingAuth: true,
+
+    checkAuth: async()=>{
+        try {
+            const res = await axiosInstance.get("/auth/check")
+
+            set({authUser: res.data})
+        } catch (error) {
+            console.log("error in checkAuth", error)
+            set({authUser: null})
+            
+        }finally{
+            set({isCheckingAuth: false})
+        }
+    },
+
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+          const res = await axiosInstance.post("/auth/signup", data);
+          set({ authUser: res.data });
+          toast.success("Account created successfully");
+          get().connectSocket();
+        } catch (error) {
+          toast.error(error.response.data.message);
+        } finally {
+          set({ isSigningUp: false });
+        }
+      },
+
+
+    
+}));
